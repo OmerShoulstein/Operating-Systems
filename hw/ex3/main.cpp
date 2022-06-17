19495f939ac92f8d62a0d2689ac1cd0fd87b459b
OmerShoulstein <omershoulstein@gmail.com>
2022-06-17 18:26:30 +0300

    added sleep to co editor

--- hw/ex3/main.cpp
+++ hw/ex3/main.cpp
@@ -55,7 +55,7 @@ void dispatcher() {
 void coEditor(int queueId) {
     while (true) {
         std::string str = coEditorQueues[queueId]->remove();
-//        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
         screenQueue->insert(str);
         if (str == "DONE") {
             break;
